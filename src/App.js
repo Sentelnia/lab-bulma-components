85ab3497b3d67323aaffdb20aa6f656cff0b3ef9
sofiaould <sofiaouldabderrahmane@yahoo.fr>
2021-06-19 14:47:20 +0200

    iteration2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import 'bulma/css/bulma.css';
 import Navbar from './navbar/Navbar.js'
+import Form from'./form/form.js'
 
 const App = () => {
   return(
     <div>
       <Navbar />
+      <Form />
     </div>
+    
   )
 };
 
